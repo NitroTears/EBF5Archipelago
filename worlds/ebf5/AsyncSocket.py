8cf29792ed6811262dd482a9ad77d75ffdbe5198
!kijetesantakalu <kijetesantakalu2@gmail.com>
2026-06-22 11:02:16 -0700

    ebf5: integrate AsyncSocket into the client and add polished connect/disconnect ebf5 commands

--- worlds/ebf5/AsyncSocket.py
+++ worlds/ebf5/AsyncSocket.py
@@ -1,37 +1,20 @@
 import asyncio
 import json
+import logging
 import selectors
-from datetime import datetime
 from inspect import iscoroutinefunction
 from socket import AF_INET, SHUT_RDWR, SO_REUSEADDR, SOCK_STREAM, SOL_SOCKET, socket
 
-
-def log(message: str):
-    now = datetime.now()
-    print(f"[{now.isoformat(' ')}] {message}")
-
+logger = logging.getLogger("Client")
 
 class EBF5AsyncSocket:
-    def __init__(self, host: str, port: int, timeouts_seconds: float = 30):
-        # Supporting IPV6 would probably be nice.
-        # But it'll probably just be running on localhost or a local network with IPV4 LAN addresses anyway.
-        self.server_sock: socket = socket(AF_INET, SOCK_STREAM)
-
-        # maybe FIXME: this should probably(?) be removed when we aren't just debug testing stuff.
-        # Maybe I'll remove it for release or if the address isn't localhost or something.
-        self.server_sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-
-        self.server_sock.bind((host, port))
-        self.server_sock.listen(1) # CHECK THIS
-        self.server_sock.setblocking(False)
+    def __init__(self, timeouts_seconds: float = 30):
+        self.server_sock: None | socket = None
 
         self.select_task: asyncio.Task | None = None
-        self.other_work_task: asyncio.Task | None = None
         self.wait_for_empty_buffers_to_close_client_socket_task: asyncio.Task | None = None
-        self.exit_event: asyncio.Event = asyncio.Event()
-
-        self.selector = selectors.DefaultSelector()
-        self.selector.register(self.server_sock, selectors.EVENT_READ, data=self.accept_connection_callback)
+        #self.exit_event: asyncio.Event = asyncio.Event()
+        self.client_disconnect_event = asyncio.Event()
 
         self.client_sock: None | socket = None
 
@@ -44,55 +27,99 @@ class EBF5AsyncSocket:
 
         self.timeouts_seconds = timeouts_seconds
 
-        if timeouts_seconds >= 0:
+    async def start(self, host: str, port: int):
+        if self.does_server_exist():
+            self.disconnect_server()
+        # should automatically disconnecting the client here just be removed?
+        if self.does_client_exist():
+            logger.info("Disconnecting EBF5...")
+            self.schedule_client_disconnect()
+            await self.client_disconnect_event.wait()
+
+        self.client_disconnect_event.clear()
+        self._clear_buffers()
+        # Supporting IPV6 would probably be nice.
+        # But it'll probably just be running on localhost or a local network with IPV4 LAN addresses anyway.
+        self.server_sock = socket(AF_INET, SOCK_STREAM)
+
+        # maybe FIXME: this should probably(?) be removed when we aren't just debug testing stuff.
+        # Maybe I'll remove it for release or if the address isn't localhost or something.
+        self.server_sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+
+        try:
+            self.server_sock.bind((host, port))
+        except Exception as e:
+            #logger.debug("Error trying to bind socket. Did you enter a valid address?\nError: %s", e, exc_info=True)
+            logger.error(f"Invalid address. Error: {e}")
+            self.server_sock.close()
+            self.server_sock = None
+            return
+
+        self.server_sock.listen(1)
+        self.server_sock.setblocking(False)
+        logger.info(f"Started listening for connections from EBF5 on {host}:{port}.")
+        self.select_task = asyncio.create_task(self.select_loop(), name="EBF5AP socket select loop")
+
+        if self.timeouts_seconds >= 0:
             self.server_sock.settimeout(self.timeouts_seconds)
 
-    def does_client_socket_exist(self) -> bool:
-        return isinstance(self.client_sock, socket) and self.client_sock.fileno() != -1
+        self.selector = selectors.DefaultSelector()
+        self.selector.register(self.server_sock, selectors.EVENT_READ, data=self.accept_connection_callback)
 
-    def add_UTF8_message_to_send_queue(self, UTF8_message: str) -> bool:
+        #await self.exit_event.wait()
+
+    def _does_socket_exist(self, socket_) -> bool:
+        return isinstance(socket_, socket) and socket_.fileno() != -1
+
+    def does_server_exist(self):
+        return self._does_socket_exist(self.server_sock)
+
+    def does_client_exist(self):
+        return self._does_socket_exist(self.client_sock)
+
+    def add_utf8_message_to_send_queue(self, utf8_message: str) -> bool:
         """Add a message to the internal message send queue, to be sent later by the select loop.\n
         ## Note: messages will not be added to the queue if `self.disconnect_scheduled` is `True`!\n
         Messages also will not be added to the queue if the client socket doesn't exist.
 
         :return: Whether the message was added to the internal message send queue or not.
         """
-        if self.disconnect_scheduled or not self.does_client_socket_exist():
-            log(f"**NOT** adding to send queue: {UTF8_message}, socket is closing or doesn't exist")
+        if self.disconnect_scheduled or not self.does_client_exist():
+            logger.debug(f"**NOT** adding to send queue: {utf8_message}, socket is closing or doesn't exist")
             return False
 
-        log(f"adding to send queue: {UTF8_message}")
-        message_bytes = UTF8_message.encode("utf-8")
+        logger.debug(f"adding to send queue: {utf8_message}")
+        message_bytes = utf8_message.encode("utf-8")
         length_bytes = len(message_bytes).to_bytes(4, "big", signed=False)
         self.message_bytes_to_send += length_bytes
         self.message_bytes_to_send += message_bytes
         return True
 
     def accept_connection_callback(self, sock: socket, mask):
-        if self.does_client_socket_exist():
-            log("something attempted to connect while the previous client socket connection is still alive, "
-                "(effectively) rejecting...")
+        if self.does_client_exist():
+            logger.info("something attempted to connect while the previous EBF5 connection is still alive."
+                " Current connection will be kept and new connection will be closed.")
             instant_disconnecting_socket, _ = sock.accept()
             instant_disconnecting_socket.shutdown(SHUT_RDWR)
             instant_disconnecting_socket.close()
             return
         self.disconnect_scheduled = False
-        self.client_sock, address = sock.accept()
-        log(f"accepting connection from {address}")
+        self.client_sock, _address = sock.accept()
+        logger.info("Accepting connection from EBF5.")
         self.client_sock.setblocking(False)
         self.selector.register(self.client_sock, selectors.EVENT_READ | selectors.EVENT_WRITE,
                                 data=self.client_readwrite_callback)
 
     async def client_readwrite_callback(self, client: socket, mask):
-        if mask & selectors.EVENT_READ and self.does_client_socket_exist():
-            log("reading...")
+        if mask & selectors.EVENT_READ and self.does_client_exist():
+            logger.debug("reading...")
 
-            new_bytes = client.recv((2 ** 16)-1)
+            new_bytes = client.recv((2 ** 16) - 1)
             self.received_message_bytes += new_bytes
             done_reading = False
 
             if len(new_bytes) == 0:
-                log("DISCONNECT RECEIVED!")
+                logger.info("EBF5 disconnected from the client.")
                 self._disconnect_client()
 
             #log(f"start: {self.received_message_bytes}")
@@ -122,34 +149,34 @@ class EBF5AsyncSocket:
                 if self.is_waiting_for_new_message and len(self.received_message_bytes) < 4:
                     done_reading = True
 
-        elif not self.does_client_socket_exist():
-            log("skipping reading because the client socket apparently doesn't exist.")
+        elif not self.does_client_exist():
+            logger.debug("skipping reading because the client socket apparently doesn't exist.")
 
-        if mask & selectors.EVENT_WRITE and self.does_client_socket_exist():
+        if mask & selectors.EVENT_WRITE and self.does_client_exist():
             if len(self.message_bytes_to_send) > 0:
-                log("writing...")
-                log(f"len(self.message_bytes_to_send): {len(self.message_bytes_to_send)}")
+                logger.debug("writing...")
+                logger.debug(f"len(self.message_bytes_to_send): {len(self.message_bytes_to_send)}")
                 bytes_sent = client.send(self.message_bytes_to_send)
                 self.message_bytes_to_send = self.message_bytes_to_send[bytes_sent:]
-                log(f"bytes_sent: {bytes_sent}, len(self.message_bytes_to_send): {len(self.message_bytes_to_send)}")
+                logger.debug(f"bytes_sent: {bytes_sent}, len(self.message_bytes_to_send): {len(self.message_bytes_to_send)}")
 
-        elif not self.does_client_socket_exist():
-            log("skipping writing because the client socket apparently doesn't exist.")
+        elif not self.does_client_exist():
+            logger.debug("skipping writing because the client socket apparently doesn't exist.")
 
     def on_message_received(self, message: str):
-        log(f"received message: \"{message}\"")
+        logger.debug(f"received message: \"{message}\"")
         reply = "message received successfully in a callback called from an asyncio `Task` polling the socket with `select()`!"
-        self.add_UTF8_message_to_send_queue(json.dumps({"type":"client_to_game_debug_message", "text":reply}))
-        self.add_UTF8_message_to_send_queue(json.dumps({"type":"client_to_game_debug_message",
+        self.add_utf8_message_to_send_queue(json.dumps({"type":"client_to_game_debug_message", "text":reply}))
+        self.add_utf8_message_to_send_queue(json.dumps({"type":"client_to_game_debug_message",
                 "text":"also unicode test: here's an emdash — mid message, emdash at the end of the message—"}))
-        self.add_UTF8_message_to_send_queue(json.dumps({"type":"client_to_game_debug_message",
+        self.add_utf8_message_to_send_queue(json.dumps({"type":"client_to_game_debug_message",
                 "text":"more random unicode characters: pi: π, smiley: 😀, pirate flag: 🏴‍☠️, all of them next to each other: π😀🏴‍☠️—"}))
-        self.schedule_client_disconnect()
+        #self.schedule_client_disconnect()
 
     def schedule_client_disconnect(self):
-        log("Client disconnect is being scheduled.")
+        logger.debug("Client disconnect is being scheduled.")
         # Special raw UTF-8 non-JSON message.
-        self.add_UTF8_message_to_send_queue("client_disconnect_soon")
+        self.add_utf8_message_to_send_queue("client_disconnect_soon")
         self.disconnect_scheduled = True
 
         self.wait_for_empty_buffers_to_close_client_socket_task = asyncio.create_task(
@@ -162,58 +189,68 @@ class EBF5AsyncSocket:
         wait_seconds_per_wait = 1/30 # 1 EBF5 frame
         waited_seconds = 0
 
-        while self.does_client_socket_exist() and (len(self.message_bytes_to_send) > 0 or len(self.received_message_bytes) > 0):
+        while self.does_client_exist() and (len(self.message_bytes_to_send) > 0 or len(self.received_message_bytes) > 0):
             await asyncio.sleep(wait_seconds_per_wait)
             waited_seconds += wait_seconds_per_wait
             if waited_seconds > max_wait_seconds:
-                log("WARNING: A client socket disconnect was scheduled but the buffers didn't clear after waiting"
-                   f"a timeout of {max_wait_seconds}, so we're force closing the socket right now anyway.")
+                logger.debug("WARNING: A client socket disconnect was scheduled but the buffers didn't clear after"
+                   f" waiting a timeout of {max_wait_seconds}, so we're force closing the socket right now anyway.")
                 #log(f"self.message_bytes_to_send: {self.message_bytes_to_send} |"
                 #    " self.received_message_bytes: {self.received_message_bytes}")
-                log(f"len(self.message_bytes_to_send): {len(self.message_bytes_to_send)} |"
-                    " len(self.received_message_bytes): {len(self.received_message_bytes)}")
-                log("(there should be nothing to send but there probably is something to read if the game is sending"
-                    "too many messages to us)")
+                logger.debug(f"len(self.message_bytes_to_send): {len(self.message_bytes_to_send)} |"
+                    f" len(self.received_message_bytes): {len(self.received_message_bytes)}")
+                logger.debug("(there should be nothing to send but there probably is something to read if the game is"
+                    "sending too many messages to us)")
                 break
 
         self._disconnect_client()
 
     def _clear_buffers(self):
-        log("clearing buffers.")
+        logger.debug("clearing buffers.")
         self.message_bytes_to_send.clear()
         self.received_message_bytes.clear()
         self.incoming_message_final_length = 0
+        self.is_waiting_for_new_message = True
 
     def _disconnect_client(self):
         """Clears the read/write buffers and closes the client socket if it currently exists.
 
         Use `schedule_client_disconnect()` instead of directly calling `_disconnect_client()` when sending messages.
         """
+        self.client_disconnect_event.set()
         self._clear_buffers()
-        if self.does_client_socket_exist():
-            log("closing client.")
+        if self.does_client_exist():
+            logger.debug("closing client.")
             self.selector.unregister(self.client_sock)
             self.client_sock.shutdown(SHUT_RDWR)
             self.client_sock.close()
         else:
-            log("client was already closed.")
+            logger.debug("client was already closed.")
 
-    def __close(self):
-        log("closing everything...")
-        self.exit_event.set()
+    def disconnect_server(self):
+        if self.does_server_exist():
+            logger.debug("disconnecting server")
+            self.selector.unregister(self.server_sock)
+            self.server_sock.shutdown(SHUT_RDWR)
+            self.server_sock.close()
+        else:
+            logger.debug("_disconnect_server() when server was already closed.")
+
+    def __full_close_immediately(self):
+        logger.debug("closing everything...")
+        #self.exit_event.set()
 
         # _disconnect_client() clears the buffers anyway
         # self._clear_buffers()
         self._disconnect_client()
-
-        self.selector.unregister(self.server_sock)
-        self.server_sock.shutdown(SHUT_RDWR)
-        self.server_sock.close()
+        self.disconnect_server()
+        # if self.does_socket_exist(self.server_sock):
+        #     self.selector.unregister(self.server_sock)
+        #     self.server_sock.shutdown(SHUT_RDWR)
+        #     self.server_sock.close()
 
         if self.select_task is not None:
             self.select_task.cancel()
-        if self.other_work_task is not None:
-            self.other_work_task.cancel()
 
         if self.wait_for_empty_buffers_to_close_client_socket_task is not None:
             self.wait_for_empty_buffers_to_close_client_socket_task.cancel()
@@ -228,8 +265,6 @@ class EBF5AsyncSocket:
                     # This is a kinda clever design but it's also confusing until you know that's what it is doing.
                     # I don't know why the library was made like this but whatever sure fine I guess.
                     # I'll just do it like this with this comment explaining it because it is admittedly convenient.
-
-                    # maybe this callback should be wrapped in a try except?
                     callback = selector_key.data
                     if callable(callback):
                         if iscoroutinefunction(callback):
@@ -240,28 +275,16 @@ class EBF5AsyncSocket:
                 await asyncio.sleep(1/30) # 1 EBF5 frame (maybe sleep less than that?)
         except (Exception, asyncio.CancelledError) as e: # Exception does not include asyncio.CancelledError apparently.
             if isinstance(e, asyncio.CancelledError):
-                log("select_loop() received `asyncio.CancelledError`, closing everything...")
+                logger.debug("select_loop() received `asyncio.CancelledError`, closing everything...")
             else:
-                log("select_loop() threw an error, closing everything...")
+                logger.debug("select_loop() threw an error, closing everything...")
 
-            self.__close()
+            self.__full_close_immediately()
 
-            log("re-raising original error.")
+            logger.debug("re-raising original error.")
             raise e
         finally:
-            log("select_loop() exiting, socket will no longer send/receive if still alive.")
-
-    async def other_work(self):
-        while True:
-            log("print from a task that'd be doing something else (+ sleep)... ")
-            await asyncio.sleep(0.1)
-
-    async def start(self):
-        log("starting server and other work task.")
-        self.select_task = asyncio.create_task(self.select_loop(), name="EBF5AP socket select loop")
-        self.other_work_task = asyncio.create_task(self.other_work(), name="EBF5AP asyncio test other work")
-
-        await self.exit_event.wait()
+            logger.debug("select_loop() exiting, socket will no longer send/receive if still alive.")
 
 # server_test = EBF5AsyncSocket("localhost", 4999)
 # asyncio.run(server_test.start())
--- worlds/ebf5/Client.py
+++ worlds/ebf5/Client.py
@@ -2,38 +2,90 @@ from __future__ import annotations
 
 import asyncio
 
-from CommonClient import ClientCommandProcessor, CommonContext, get_base_parser, gui_enabled, server_loop
+from CommonClient import ClientCommandProcessor, CommonContext, get_base_parser, gui_enabled, logger, server_loop
 
 from .AsyncSocket import EBF5AsyncSocket
 
 
-# We're gonna need at least a custom command or two to connect to the game.
 class EBF5CommandProcessor(ClientCommandProcessor):
     def __init__(self, ctx: EBF5Context):
-        super().__init__(ctx)
+        self.ctx = ctx
 
     def _cmd_ebf5launchercomponenttest(self):
         """Test command."""
         self.output("_cmd_ebf5launchercomponenttest() was called successfully!")
 
-    def _cmd_debug_unlock_item_test(self, item_AP_name: str = ""):
+    def _cmd_debug_unlock_item_test(self, client_item_name: str = ""):
         """
-        Unlock an item in game, only for testing/debugging the client. This does not interface with AP at all.
+        Unlock an item in game, only for testing/debugging the client. This *does not* interface with AP at all.
         """
-        if item_AP_name == "":
+        if client_item_name == "":
             self.output("specify an item name")
             return False
 
         self.output("unimplemented")
         return False
 
+    async def _cmd_connect_ebf5(self, address: str = ""):
+        """
+        Start listening on `address` for incoming connections from EBF5. EBF5 will be disconnected if it is connected
+        when this command is used.
+
+        :param address: IPv4 address to start listening on. Defaults to `localhost:4999` if unspecified.
+        """
+        if address == "":
+            address = "localhost:4999"
+        split_address = address.split(":")
+        # This won't cover every edge case with the IPv4 address but it'll cover some of them.
+        # If the address is can't be bound, the AsyncSocket will print a message to the user.
+        if len(split_address) != 2:
+            self.output("Please provide an IPv4 address in the form of `host:port`.")
+            return False
+
+        host, port = split_address
+
+        try:
+            port = int(port)
+        except ValueError:
+            self.output("Please use an integer port number.")
+            return False
+
+        if port < 0 or port > 65535:
+            logger.info("port numbers must be positive integers below 65535.")
+            return False
+        if port < 1024:
+            logger.warning("Warning: Flash Player places restrictions on connecting to ports below 1024."
+                            " EBF5 might not be able to connect to this port.")
+
+        await self.ctx.ebf5_socket.start(host, port)
+        return True
+
+    async def _cmd_disconnect_ebf5(self):
+        """
+        Disconnect EBF5 from the client if connected and stop listening for connections from EBF5.
+        """
+        if self.ctx.ebf5_socket.does_client_exist():
+            logger.info("disconnecting EBF5...")
+            self.ctx.ebf5_socket.schedule_client_disconnect()
+        else:
+            logger.info("EBF5 already wasn't connected to the client.")
+
+        if self.ctx.ebf5_socket.does_server_exist():
+            self.ctx.ebf5_socket.disconnect_server()
+            logger.info("stopped listening for connections from EBF5.")
+        else:
+            logger.info("Client already wasn't listening for connections from EBF5.")
+
+        return True
+
 class EBF5Context(CommonContext):
     command_processor = EBF5CommandProcessor
-    ebf5_socket: EBF5AsyncSocket | None = None
 
     # TODO: implement ebf5_address here after i get the command for it working.
-    def __init__(self, server_address: str | None = None, password: str | None = None) -> None:
+    def __init__(self, server_address: str | None = None, password: str | None = None, ebf5_address: str | None = None) -> None:
         super().__init__(server_address, password)
+        #self.ebf5_address: str | None = ebf5_address
+        self.ebf5_socket: EBF5AsyncSocket = EBF5AsyncSocket()
 
 # General plan:
 # 1. Add a launcher integration and make code run when you press launch.
